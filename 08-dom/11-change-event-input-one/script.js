8a0b8e9f66f44e831452755c785643a94d88e9f3
Julie <juptsr.becode@gmail.com>
2022-03-02 15:31:01 +0100

    11 from dom

--- 08-dom/11-change-event-input-one/script.js
+++ 08-dom/11-change-event-input-one/script.js
@@ -4,4 +4,51 @@
 (() => {
 
     // your code here
+
+    let input = document.getElementById("pass-one");
+    let span = document.getElementById("counter");
+    let count = 0;
+
+    input.addEventListener("input", (e) => {
+        console.log("ok");
+
+        count++;
+
+        if(count > 10){
+            console.log("no more input");
+        }
+
+        span.innerHTML = `${count}/10`;
+        console.log(count);
+    });
+
+    input.addEventListener("keydown", (e) => {
+        const key = e.key;
+        if(key === "Backspace"){
+            count--;
+            span.innerHTML = `${count}/10`;
+            console.log(`Count = ${count}`);
+        }
+    });
+
+    // input.addEventListener("input", whileInput);
+    // input.addEventListener("keydown", whileInput);
+
+    // function whileInput(e){
+    //     console.log("ok");
+
+    //     count++;
+
+    //     const key = e.key;
+    //     if(key === "Backspace"){
+    //         console.log("backspace");
+    //     }
+
+    //     if(count > 10){
+    //         console.log("no more input");
+    //     }
+
+    //     span.innerHTML = `${count}/10`;
+    //     console.log(count);
+    // }
 })();
