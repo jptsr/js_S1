4bca7f5f69021e293869bb008d9b3be6e48ef200
Julie <juptsr.becode@gmail.com>
2022-03-03 16:55:36 +0100

    1 from async

--- 10-asynchronous/01-get-posts/script.js
+++ 10-asynchronous/01-get-posts/script.js
@@ -3,4 +3,26 @@
 
 (() => {
     // your code here
+
+    /*
+    !!! getPosts est déjà "défini / paramétré" !!!
+    On renomme les paramètres cô on veut. Leur manière de fctionner est déjà définie avec la fct
+
+    window.lib.getPosts((pError, pArr) => {
+        // pError "retourne" null
+        // pArr "retourne" Arr. ... 
+        voir shared/lib/callback.js 
+    });
+    */
+
+    document.getElementById('run').addEventListener('click', () => {
+
+        window.lib.getPosts((pError, pArr) => {
+            console.clear();
+    
+            
+        });
+
+    });
+
 })();
